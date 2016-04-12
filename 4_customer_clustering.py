c6cc4079cc93093c8b69ef6adc99eb62bc12d8ba
letfly <letfly@outlook.com>
2016-04-12 19:11:49 +0800

    利用层次聚类算法对客户分群_k均值

--- 4_customer_clustering.py
+++ 4_customer_clustering.py
@@ -1,26 +1,66 @@
 from numpy import *
 
-def loadDataSet(fileName):
-    dataMat = []
+
+def loadDataList(fileName):
+    dataList = []
     fr = open(fileName)
     for line in fr.readlines():
         curLine = line.strip().split('\t')
         fltLine = map(float, curLine)
-        dataMat.append(fltLine)
-    return dataMat
+        dataList.append(fltLine)
+    return dataList
 
-def distEclud(vecA, vecB):
-    return sqrt(sum(power(vecA-vecB, 2)))
 
-def randCent(dataSet, k):
-    n = shape(dataSet)[1]
-    centroids = mat(zeros((k,n)))
+def randCent(dataMat, k):
+    n = shape(dataMat)[1]
+    centroids = mat(zeros((k, n)))
     for j in range(n):
-        minJ = min(dataSet[:,j])
-        rangeJ = float(max(dataSet[:,j])-minJ)
-        centroids[:,j]=minJ+rangeJ*random.rand(k,1)
+        minJ = min(dataMat[:, j])
+        rangeJ = float(max(dataMat[:, j])-minJ)
+        centroids[:, j] = minJ + rangeJ*random.rand(k, 1)
+        print rangeJ, centroids
     return centroids
 
+'''
 if __name__ == '__main__':
     from numpy import *
-    dataMat=mat(loadDataSet('testSet.txt'))
+    dataMat=mat(loadDataList('testSet.txt'))
+    print min(dataMat[:,0])
+    print min(dataMat[:,1])
+    print max(dataMat[:,1])
+    print max(dataMat[:,0])
+    print randCent(dataMat, 2)
+'''
+
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2)))
+
+
+def kMeans(dataMat, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataMat)[0]
+    clusterAssment = mat(zeros((m, 2)))
+    centroids = createCent(dataMat, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = inf
+            minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j, :], dataMat[i, :])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist**2
+        print centroids
+        for cent in range(k):
+            ptsInClust = dataMat[nonzero(clusterAssment[:, 0].A == cent)[0]]
+            centroids[cent, :] = mean(ptsInClust, axis=0)
+    return centroids, clusterAssment
+
+if __name__ == '__main__':
+    dataMat = mat(loadDataList('testSet.txt'))
+    print kMeans(dataMat, 4)
