785294d00a9cad45caf330799f418a9599a90ca4
letfly <letfly@outlook.com>
2016-04-15 10:16:25 +0800

    BP神经网络应用

---
+++ 3_emotional_tendency/handwritten_digits.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from sklearn.datasets import load_digits
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import LabelBinarizer
+from neural_network import NeuralNetwork
+from sklearn.cross_validation import train_test_split
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+X -= X.min()
+X /= X.max()
+
+nn = NeuralNetwork([64, 10, 10], 'logistic')
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+labels_train = LabelBinarizer().fit_transform(y_train)
+labels_test = LabelBinarizer().fit_transform(y_test)
+print "start fitting"
+nn.fit(X_train, labels_train, epochs=3000)
+predictions = []
+for i in xrange(X_test.shape[0]):
+    o = nn.predict(X_test[i])
+    predictions.append(np.argmax(o))
+print confusion_matrix(y_test, predictions)
+print classification_report(y_test, predictions)
+
+import pylab as pl
+pl.matshow(digits.images[0])
+pl.show()
