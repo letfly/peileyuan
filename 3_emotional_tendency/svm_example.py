528893534795363f65ebb5ba3beebe950ddf8bf2
letfly <letflykid@gmail.com>
2016-04-18 22:27:08 +0800

    svm应用上(2)

---
+++ 3_emotional_tendency/svm_example.py
@@ -0,0 +1,46 @@
+print(__doc__)
+
+import numpy as np
+import pylab as pl
+from sklearn import svm
+
+# we create 40 separable points
+np.random.seed(0)
+X = np.r_[np.random.randn(20, 2)-[2,2],np.random.randn(20, 2)+[2, 2]]
+Y = [0] * 20 + [1] * 20
+
+# fit the model
+clf = svm.SVC(kernel='linear')
+clf.fit(X, Y)
+
+# get the separating hyperplane
+w = clf.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(-5, 5)
+yy = a * xx - (clf.intercept_[0]) / w[1]
+
+# plot the parallels to the separating hyperplane that pass through the
+# support vectors
+b = clf.support_vectors_[0]
+yy_down = a * xx + (b[1] - a * b[0])
+b = clf.support_vectors_[-1]
+yy_up = a * xx + (b[1] - a * b[0])
+
+print "w: ", w
+print "a: ", a
+# print "xx:"
+print "support_vectors_: ", clf.support_vectors_
+print "clf.coef_: ", clf.coef_
+
+# switching to the generic can be 
+
+
+pl.plot(xx, yy, 'k-')
+pl.plot(xx, yy_down, 'k--')
+pl.plot(xx, yy_up, 'k--')
+
+pl.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
+           s=80, facecolors='none')
+pl.scatter(X[:, 0], X[:, 1], c=Y, cmap=pl.cm.Paired)
+
+pl.show()
